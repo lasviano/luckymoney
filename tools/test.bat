fd95dc83785b5f37bdef9e827e1e700dd03bca3f
xuwb-b <xuwb-b@glodon.com>
2019-12-20 15:12:29 +0800

    add tools/test.bat

---
+++ tools/test.bat
@@ -0,0 +1,13 @@
+set curWorkPath=%~dp0
+set bullseyeResultPath=%COVFILE%
+set trunkPath=%curWorkPath%\..\..\..\
+
+pause
+
+git pull
+
+@echo StartTime:> startTime.txt
+@echo %date%>> startTime.txt
+@echo %time%>> startTime.txt
+
+pause
